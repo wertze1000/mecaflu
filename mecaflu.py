7f809cc2a7ef2899c2c3604d76411e3609fb9aaa
wertze1000 <99504716+wertze1000@users.noreply.github.com>
2023-03-23 16:48:07 +0100

    Update mecaflu.py

--- mecaflu.py
+++ mecaflu.py
@@ -1,15 +1,33 @@
 import numpy as np
-#getcoeff()#4 noeuds (gauche droite haut bas), renvoie une matrice de coefficients a dans un éq Ax = b
 #laplace()#3 matrices DOM, NUM, CL, retourne une matrice psy
 
 def getCoeff(num_left, num_right, num_down, num_up, num_cent, type_cent, cl_cent):
      #cefficients
-    j = np.array() #phi
-    b = np.array() #b
     
-    if(cl_cent == 1) {
-        A = np.array([[1],[1],[1],[1],[-4]])
-        print(A)
+
+    if(cl_cent == 1):
+        A = np.array([[1],[1],[1],[1],[-4]], dtype = int)
+        j = np.array([[num_left], [num_right], [num_down], [num_up], [num_cent]], dtype = int)
+        b = 0
+        #print("Bonsoir 1")
+        #print(type(A))
+        #print(A)
+        #print(j)
         
-    }
+        
+    elif(cl_cent == 2):
+        A = np.array([[1]], dtype = int)
+        j = np.array([[num_cent]], dtype = int)
+        b = cl_cent
+        #print("Bonsoir 2")
+        #print(type(A))
+
+    elif(cl_cent == 0):
+        A, j = np.zeros(1)
+        b = 0
+        #print("Bonsoir 3")
+        
+    
     return A, j, b
+
+#getCoeff(0,0,0,0,0,0,1)
