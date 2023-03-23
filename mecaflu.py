4d3a04f6c4c70c58c479e8499efec5284f9ca11f
wertze1000 <99504716+wertze1000@users.noreply.github.com>
2023-03-23 21:21:13 +0100

    Update mecaflu.py

--- mecaflu.py
+++ mecaflu.py
@@ -8,7 +8,7 @@ CL_D = np.loadtxt('./data/1-cl.txt', dtype = int)
 
 def getCoeff(num_left, num_right, num_down, num_up, num_cent, type_cent, cl_cent):
      #cefficients
-    
+
 
     if(type_cent == 1):
         A = np.array([[1],[1],[1],[1],[-4]], dtype = int)
@@ -16,8 +16,8 @@ def getCoeff(num_left, num_right, num_down, num_up, num_cent, type_cent, cl_cent
         b = 0
         print(A)
         print(j)
-        
-        
+
+
     elif(type_cent == 2):
         A = np.array([[1]], dtype = int)
         j = np.array([[num_cent]], dtype = int)
@@ -30,28 +30,36 @@ def getCoeff(num_left, num_right, num_down, num_up, num_cent, type_cent, cl_cent
     else:
         A, j = np.zeros(2)
         b = 0
-    
+
     return j, A, b
 
 def Laplace(dom, num, cl_d):
+    length_b = (len(dom) - 2)*(len(dom[0]) - 2)
+    b_vec = np.zeros(length_b + 1)
+
     for i in range(len(dom[0])):
         for j in range(len(dom)):
-            if(dom(i,j) == 0):
+            if(dom[i,j] == 0):
                 continue
             else:
-                k, A, b = getCoeff(num(i - 1, j), num(i + 1, j), num(i, j - 1), num(i, j + 1), num(i, j), dom(i, j), cl_d(i, j))
-                A_full = np.append(A)
-                k_full = np.append(k)
+                A_full = []
+                k_full = []
+                line_full = []
 
+                k, A, b = getCoeff(num[i - 1, j], num[i + 1, j], num[i, j - 1], num[i, j + 1], num[i, j], dom[i, j], cl_d[i, j])
+                A_full.append(A)
+                k_full.append(k)
                 line = A
+
+                b_vec[num[i,j]] = cl_d[i,j]
                 for l in range(len(A[0])):
-                    line[l] = num(i,j)
-                
-                line_full = np.append(line)
+                    line[l] = num[i,j]
 
-    A_sparse = csc_matrix(A_full)
+                line_full.append(line)
+
+    print(A_full, k_full, line_full)
+    A_sparse = csc_matrix((A_full, (line_full, k_full)))
     print(A_sparse)
+    return A_sparse
 
-
-Laplace(DOM, NUM, CL_D)
-                
+Sparse = Laplace(DOM, NUM, CL_D)
