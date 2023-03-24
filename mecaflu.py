52be49030378568201ec6c0f86c89c8753f6aebf
wertze1000 <99504716+wertze1000@users.noreply.github.com>
2023-03-24 15:57:16 +0100

    Update mecaflu.py
    Laplace marche

--- mecaflu.py
+++ mecaflu.py
@@ -1,27 +1,22 @@
 import numpy as np
 from scipy.sparse import csc_matrix
 from scipy.sparse import linalg
-#laplace()#3 matrices DOM, NUM, CL, retourne une matrice psy
 
 NUM = np.loadtxt('./data/1-num.txt', dtype = int)
 DOM = np.loadtxt('./data/1-dom.txt', dtype = int)
 CL_D = np.loadtxt('./data/1-cl.txt', dtype = float)
 
 def getCoeff(num_left, num_right, num_down, num_up, num_cent, type_cent, cl_cent):
-     #cefficients
-    
 
     if(type_cent == 1):
-        A = np.array([[1],[1],[1],[1],[-4]], dtype = int)
+        A = np.array([[1], [1], [1], [1], [-4]], dtype = int)
         j = np.array([[num_left], [num_right], [num_down], [num_up], [num_cent]], dtype = int)
         b = 0
-        
         
     elif(type_cent == 2):
         A = np.array([[1]], dtype = int)
         j = np.array([[num_cent]], dtype = int)
         b = cl_cent
-        #print(type(A))
 
     elif(type_cent == 0):
         A, j = np.zeros(2)
@@ -33,44 +28,48 @@ def getCoeff(num_left, num_right, num_down, num_up, num_cent, type_cent, cl_cent
     return j, A, b
 
 def Laplace(dom, num, cl_d):
-    b_vec   = np.array([], dtype = float)
-    b       = np.array([0.0], dtype = float)
+
+    b_vec   = np.array([], dtype = float) #float!
     n_vec   = np.array([], dtype = int)
-    n       = np.array([0], dtype = int)
-    #[] pour pas avoir "built in function et dtype pour avoir des int car spare prend que Ã§a
-    A_full = np.array([], dtype = int)
+    
+    A_full = np.array([], dtype = int) #[] pour pas avoir "built in function" et dtype pour avoir des int car sparse prend que int.
     k_full = np.array([],dtype = int)
     line_full = np.array([], dtype = int)
-    for i in range(len(dom[0])):
-        for j in range(len(dom)):
-             if(dom[i,j] == 0):
+    
+    for i in range(len(dom[0])): #iteration sur les lignes
+
+        for j in range(len(dom)):   #iteration sur les colonnes
+             
+             if(dom[i,j] == 0): #si on arrive à un zéro on skip
                 continue
-             else:
+             
+             else: #(si c'est pas un zéro)
                 k, A, b = getCoeff(num[i - 1, j], num[i + 1, j], num[i, j - 1], num[i, j + 1], num[i, j], dom[i, j], cl_d[i, j])
+                
                 A_full = np.append(A_full, A)
                 k_full = np.append(k_full, k)
-                b = cl_d[i,j]
-                b_vec = np.append(b_vec,b)
-                n[0] = num[i,j] - 1
-                n_vec = np.append(n_vec,n)
-                line = A
+                b_vec = np.append(b_vec, cl_d[i,j])
+                n_vec = np.append(n_vec, num[i,j] - 1)
+
+                line = A #On a besoin de la taille de A
                 for l in range(len(A)):
                     line[l] = num[i,j]
                 
                 line_full = np.append(line_full, line)
 
-    b_sparse = csc_matrix((b_vec, (n_vec, n_vec)))
-    one = np.ones(len(b_vec))
-    print(one)
-    one_vert = one.reshape(len(b_vec),1)
-    print(one_vert)
-    b_final = csc_matrix.multiply(b_sparse, one_vert)
-    print(b_final)
-    k_full = k_full - 1
-    line_full = line_full - 1
+    #On organise les valeurs de b pour avoir un vecteur
+    b_final = np.zeros(len(b_vec), dtype = float)
+    for p in range(len(b_vec)):
+        b_final[n_vec[p]] = b_vec[p]
+
+    #On reset les indices pour que ce soit bien en matriciel
+    k_full -= 1
+    line_full -= 1
+
     A_sparse = csc_matrix((A_full, (line_full, k_full)))
+    
     return linalg.spsolve(A_sparse, b_final)
 
 
-lp=Laplace(DOM, NUM, CL_D)
+lp = Laplace(DOM, NUM, CL_D)
 print(lp)
