922eb587b928e8d38b3b5c77dd0d7b9b13c50fde
Matteo Pulvirenti <128724239+ahruno@users.noreply.github.com>
2023-04-05 14:51:22 +0200

    Delete meca_flu directory

--- meca_flu/PRESSURE.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-def norm(x,y):
-    return np.sqrt(x*x + y*y)
-
-def pressure(u,v,rho):
-    p=np.zeros(len(u), dtype=float)
-    for i in range(len(p)):
-        p[i]=rho*0.5*(norm(u[i],v[i]))*(norm(u[i],v[i]))
-    return p
