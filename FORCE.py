1ebad93482cf07e5b9f3c77d04a8fbd81eb8f511
Matteo Pulvirenti <128724239+ahruno@users.noreply.github.com>
2023-04-12 18:16:08 +0200

    Update FORCE.py

--- FORCE.py
+++ FORCE.py
@@ -2,6 +2,6 @@ def force(p,x,y):
     fx = 0.0
     fy = 0.0
     for i in range(len(x) - 1):
-        fx = fx + ((x[i + 1] - x[i]) / 2)*(p[i + 1] + p[i] )
-        fy = fy - ((y[i + 1] - y[i]) / 2)*(p[i + 1] + p[i] )
-    return fx, fy
+        fx = fx + ((y[i+1] - y[i])/2)*(p[i+1] + p[i] )
+        fy = fy - ((x[i+1] - x[i])/2)*(p[i+1] + p[i] )
+    return fx,fy
