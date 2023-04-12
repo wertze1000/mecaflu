bca7af864acc333dd9f41e8c093e58fa868e0950
Matteo Pulvirenti <128724239+ahruno@users.noreply.github.com>
2023-04-12 17:03:27 +0200

    Update DERIV.py

--- DERIV.py
+++ DERIV.py
@@ -7,8 +7,10 @@ def deriv(f_left, f_c, f_right, type_left, type_c, type_right, h):
     elif(type_c == 2):
         if(type_right == 0): #décentrée arrière(bord droit par exemple)
             v = (f_c - f_left)/h
-        else:               #décentrée avant
+        elif(type_left == 0):               #décentrée avant
             v = (f_right - f_c)/h
+        else:
+            v = (f_right - f_left)/(2 * h)
     elif(type_c == 0):
         v = 0.0
     else:
