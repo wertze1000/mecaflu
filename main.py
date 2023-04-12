aaf555bf89aa22b6470cf6ca75a04a0d0a91e856
wertze1000 <99504716+wertze1000@users.noreply.github.com>
2023-04-12 11:29:27 +0200

    com

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ from CONDITION import cl4
 #importation des matrices
 NUM = np.rot90(np.loadtxt('./data/4-num.txt', dtype = int))
 DOM = np.rot90(np.loadtxt('./data/4-dom.txt', dtype = int))
-#CL_D = np.rot90(np.loadtxt('./data/1-cl.txt', dtype = float))
+#CL_D = np.rot90(np.loadtxt('./data/2-cl.txt', dtype = float))
 CONTOUR=np.loadtxt('./data/4-contourObj.txt', dtype = int)
 
 #paramètres
@@ -21,12 +21,13 @@ rho= 1000
 ### --TEST-- ###
 CL_D = cl4(DOM,débit)
 lap, psi = Laplace(DOM, NUM, CL_D) #(psi = lap)
-print(lap)
+#print(lap)
 u, v = velocity(lap, psi, DOM, NUM, 2)
 #print("U =", u,"V =", v)
 #print(pressure(u,v,rho))
 vx, vy, vn=velocity_field(u, v, NUM)
-print(CL_D)
+#print(CL_D)
+print('VN = ', vn)
 
 ### --COLORIAGE-- ###
 fig, ax0 = plt.subplots(1, 1)
@@ -34,12 +35,14 @@ c = ax0.pcolor(vn, cmap = plt.cm.plasma)
 ax0.set_aspect('equal','box')
 
 ### --STREAMPLOT-- ###
-x_grid=CL=np.zeros(shape = DOM.shape, dtype = int)
-y_grid=CL=np.zeros(shape = DOM.shape, dtype = int)
+x_grid = CL = np.zeros(shape = DOM.shape, dtype = int)
+y_grid = CL = np.zeros(shape = DOM.shape, dtype = int)
+
 for i in range(len(DOM)):
     for j in range(len(DOM[0])):
         x_grid[i,j] = i
         y_grid[i,j] = j
+
 ax0.streamplot(y_grid, x_grid, vy, vx, color='white',density=1)
 fig.colorbar(c, ax = ax0)
 plt.show()
